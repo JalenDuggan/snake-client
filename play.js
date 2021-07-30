103a1a780b4d683cdd066174d8a69f7bbce8914c
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-07-30 08:30:14 +0000

    Deleted comments

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
 const net = require("net");
 const connect = require('./client');
 
-// establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: 'localhost',
-//     port: 50541
-//   });
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   conn.on("connect", () => {
-//     console.log("you ded cuz you idled");
-//   })
-  
-//   return conn;
-// };
 connect();
 console.log("Connecting ...");
-
