d26b9702687cac00a15211b6da6fe5f25e461e4c
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-07-30 11:23:02 +0000

    Clean up the play file by taking the input function and placing it in the input file

---
+++ input.js
@@ -0,0 +1,33 @@
+const net = require("net");
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = (input) => {
+    if (input === 'w') {
+      stdin.write("Move: up")
+      
+    }
+    if (input === 'a') {
+      stdin.write("Move: left")
+      
+    }
+    if (input === 's') {
+      stdin.write("Move: down")
+      
+    }
+    if (input === 'd') {
+      
+      stdin.write("Move: right")
+    }
+    if (input === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,38 +1,8 @@
 const net = require("net");
 const connect = require('./client');
+const setupInput = require('./input');
 
 connect();
 console.log("Connecting ...");
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = (input) => {
-    if (input === 'w') {
-      stdin.write("Move: up")
-      
-    }
-    if (input === 'a') {
-      stdin.write("Move: left")
-      
-    }
-    if (input === 's') {
-      stdin.write("Move: down")
-      
-    }
-    if (input === 'd') {
-      
-      stdin.write("Move: right")
-    }
-    if (input === '\u0003') {
-      process.exit();
-    }
-  }
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
 
 setupInput()
