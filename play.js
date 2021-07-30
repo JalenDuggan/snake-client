c13d23f28ea67552aef74b0bf31009cf4db13d39
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-07-30 11:16:09 +0000

    Added some input logic to the play file and added some WASD if statements that dont work

--- play.js
+++ play.js
@@ -3,3 +3,36 @@ const connect = require('./client');
 
 connect();
 console.log("Connecting ...");
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = (input) => {
+    if (input === 'w') {
+      stdin.write("Move: up")
+      
+    }
+    if (input === 'a') {
+      stdin.write("Move: left")
+      
+    }
+    if (input === 's') {
+      stdin.write("Move: down")
+      
+    }
+    if (input === 'd') {
+      
+      stdin.write("Move: right")
+    }
+    if (input === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+setupInput()
